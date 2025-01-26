b64c0079c6d6ca83e810eba1df5e5647fad15833
Varshini Sandepudi <varshinisandepudi@gmail.com>
2025-01-27 01:50:15 +0530

    image solved

--- src/services/index.js
+++ src/services/index.js
@@ -1,20 +1,3 @@
-{/*import axios from "axios"; */}
-
-{/*const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
-
-export const register = async (data) => {
-    const response = await fetch(`${BACKEND_URL}/api/user/signup`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-    if (response.status === 200 || response.status === 400) {
-        return response.json()
-    }
-    throw new Error('Something went wrong')
-} */}
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
@@ -42,5 +25,7 @@ export const register = async (data) => {
 };
 
 export const uploads = (imageUrl) => {
-    return `${BACKEND_URL}/images/${imageUrl}`;
-};
+    return `${import.meta.env.VITE_BACKEND_URL}/images/${imageUrl}`;
+}; 
+
+
